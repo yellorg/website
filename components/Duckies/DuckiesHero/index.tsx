ba0f0fefcb9e501b1364989d1d476a35e8d32812
akushniruk <akushniruk@openware.com>
2022-05-25 22:59:34 +0200

    feat(metamask): added modal window

--- components/Duckies/DuckiesHero/index.tsx
+++ components/Duckies/DuckiesHero/index.tsx
@@ -8,15 +8,23 @@ import useDApp from '../../../hooks/useDApp';
 import useWallet from '../../../hooks/useWallet';
 import { shortenHex } from '../../../libs/utils';
 import { useENSName } from '../../../hooks/useENSName';
+import { DuckiesConnectorModalWindow } from '../DuckiesConnectModalWindow';
 
 export const DuckiesHero = () => {
     const [isMetaMaskInstalled, setMetaMaskInstalled] = useState<boolean>(false);
+    const [isOpenConnect, setIsOpenConnect] = useState<boolean>(false);
 
     const { connectWithProvider } = useDApp();
     const { active, account, chain } = useWallet();
     const ENSName = useENSName(account);
 
     const onboarding = useRef<MetaMaskOnboarding>();
+
+    useEffect(() => {
+        if (active && account) {
+            setIsOpenConnect(false)
+        }
+    }, [active, account]);
 
     const handleConnectWallet = useCallback(
         async (provider: ProviderWhitelist) => {
@@ -57,6 +65,7 @@ export const DuckiesHero = () => {
     const handleMetamask = (isMetaMaskInstalled: boolean, id: ProviderWhitelist) => {
         if (isMetaMaskInstalled || id !== 'Injected' ) {
             handleConnectWallet(id);
+            setMetaMaskInstalled(false)
         } else {
             onboarding.current?.startOnboarding();
         }   
@@ -79,70 +88,86 @@ export const DuckiesHero = () => {
         <div onClick={() => handleMetamask(isMetaMaskInstalled, 'Injected')} className="button button--outline button--secondary button--shadow-secondary">
             <span className="button__inner">{isMetaMaskInstalled ? 'Connect Metamask' : 'Install Metamask'}</span>
         </div>
-    )
+    );
+    
+    const handleClaimReward = () => {
+        if (!active) {
+            setIsOpenConnect(true);
+        } else {
+            console.log('claim logic')
+        }
+    }
 
     return (
-        <div className="duckies-hero">
-            <div className="container">
-                <div className="duckies-hero__info">
-                    <div className="duckies-hero__info-selebration">
-                        <LazyLoadImage className="section__main-img"
-                            srcSet={`${'/images/components/duckies/selebration.svg'}`}
-                            effect="blur"
-                            threshold={200}
-                        />
-                    </div>
-                    <div className="duckies-hero__info-received">
-                        <div className="duckies-hero__info-received-message">
-                            You have received
-                        </div>
-                        <div className="duckies-hero__info-received-amount">
-                            <span>+10,000,000</span>
-                            DUCKIES
-                        </div>
-                    </div>
-                    <div className="duckies-hero__info-buttons">
-                        <div className="duckies-hero__info-buttons-claim button button--outline button--secondary button--shadow-secondary">
-                            <span className="button__inner">Claim your reward</span>
-                        </div>
-                        <Link href="#earn-more">
-                            <a className="duckies-hero__info-buttons-earn button button--secondary button--shadow-secondary">
-                                <span className="button__inner">
-                                    Earn more
-                                </span>
-                            </a>
-                        </Link>
-                    </div>
-                </div>
-                <div className="duckies-hero__icons">
-                    <div className="duckies-hero__icons-balance button--shadow-secondary">
-                        <div className="duckies-hero__icons-balance-body">
-                            <div className="duckies-hero__icons-balance-body-content">
-                                <div className="duckies-hero__icons-balance-body-content-title">
-                                    <div className="duckies-hero__icons-balance-body-content-title-name">
-                                        Balance
-                                    </div>
-                                    <div className="duckies-hero__icons-balance-body-content-title-icon">
-                                        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M11 14H10V10H9M10 6H10.01M19 10C19 14.9706 14.9706 19 10 19C5.02944 19 1 14.9706 1 10C1 5.02944 5.02944 1 10 1C14.9706 1 19 5.02944 19 10Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                                        </svg>
-                                    </div>
-                                </div>
-                                {active ? renderMetamaskAccount() : renderMetamaskButton()}
-                            </div>
-                        </div>
-                    </div>
-                    <div className="duckies-hero__icons-duck">
-                        <div className="duckies-hero__icons-duck-body">
-                            <LazyLoadImage className="duckies-hero__icons-duck-body-img"
-                                srcSet={`${'/images/components/duckies/duck.svg'}`}
+        <>
+            <div className="duckies-hero">
+                <div className="container">
+                    <div className="duckies-hero__info">
+                        <div className="duckies-hero__info-selebration">
+                            <LazyLoadImage className="section__main-img"
+                                srcSet={`${'/images/components/duckies/selebration.svg'}`}
                                 effect="blur"
                                 threshold={200}
                             />
                         </div>
+                        <div className="duckies-hero__info-received">
+                            <div className="duckies-hero__info-received-message">
+                                You have received
+                            </div>
+                            <div className="duckies-hero__info-received-amount">
+                                <span>+10,000,000</span>
+                                DUCKIES
+                            </div>
+                        </div>
+                        <div className="duckies-hero__info-buttons">
+                            <div onClick={handleClaimReward} className="duckies-hero__info-buttons-claim button button--outline button--secondary button--shadow-secondary">
+                                <span className="button__inner">Claim your reward</span>
+                            </div>
+                            <Link href="#earn-more">
+                                <a className="duckies-hero__info-buttons-earn button button--secondary button--shadow-secondary">
+                                    <span className="button__inner">
+                                        Earn more
+                                    </span>
+                                </a>
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="duckies-hero__icons">
+                        <div className="duckies-hero__icons-balance button--shadow-secondary">
+                            <div className="duckies-hero__icons-balance-body">
+                                <div className="duckies-hero__icons-balance-body-content">
+                                    <div className="duckies-hero__icons-balance-body-content-title">
+                                        <div className="duckies-hero__icons-balance-body-content-title-name">
+                                            Balance
+                                        </div>
+                                        <div className="duckies-hero__icons-balance-body-content-title-icon">
+                                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M11 14H10V10H9M10 6H10.01M19 10C19 14.9706 14.9706 19 10 19C5.02944 19 1 14.9706 1 10C1 5.02944 5.02944 1 10 1C14.9706 1 19 5.02944 19 10Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                                            </svg>
+                                        </div>
+                                    </div>
+                                    {active ? renderMetamaskAccount() : renderMetamaskButton()}
+                                </div>
+                            </div>
+                        </div>
+                        <div className="duckies-hero__icons-duck">
+                            <div className="duckies-hero__icons-duck-body">
+                                <LazyLoadImage className="duckies-hero__icons-duck-body-img"
+                                    srcSet={`${'/images/components/duckies/duck.svg'}`}
+                                    effect="blur"
+                                    threshold={200}
+                                />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <DuckiesConnectorModalWindow
+                bodyContent={active ? <div>Claim logic</div> : renderMetamaskButton()}
+                headerContent="Connect Wallet"
+                isOpen={isOpenConnect}
+                setIsOpen={setIsOpenConnect}
+            />
+        </>
     );
 };
