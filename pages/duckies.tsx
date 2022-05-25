941d4c9e0bcaa6d1e948a3d979b6151543d6e517
Oleksandr Khlopiachyi <akhlopiachyi@openware.com>
2022-05-26 01:18:04 +0700

    feat: add provider and settings for metamask connection

--- pages/duckies.tsx
+++ pages/duckies.tsx
@@ -6,31 +6,42 @@ import { DuckiesHero} from '../components/Duckies/DuckiesHero';
 import { DuckiesAffiliates} from '../components/Duckies/DuckiesAffiliates';
 import { DuckiesEarnMore} from '../components/Duckies/DuckiesEarnMore';
 import { DuckiesRedeem} from '../components/Duckies/DuckiesRedeem';
+import { Web3ReactProvider } from '@web3-react/core';
+import { ethers } from 'ethers';
+import type { AppProps } from 'next/app';
 
+const getLibrary = (provider: any): ethers.providers.Web3Provider => {
+    const library = new ethers.providers.Web3Provider(provider, 'any');
+    library.pollingInterval = 12000;
+
+    return library;
+};
 
 const Home: FC<{}> = (): JSX.Element => {
     return (
-        <div className="flex flex-col min-h-full">
-            <Navbar />
-            <div className="main-wrapper">
-                <Head>
-                    <title>
-                        Yellow DeFi - Discover WEB 3.0 Internet of Finance
-                    </title>
-                    <meta
-                        name="description"
-                        content="Yellow DeFi is a new generation hybrid technology cryptocurrency exchange combining the best of decentralized and centralized performance."
-                    />
-                </Head>
-                <main className="duckies container">
-                    <DuckiesHero />
-                    <DuckiesAffiliates />
-                    <DuckiesEarnMore />
-                    <DuckiesRedeem />
-                </main>
+        <Web3ReactProvider getLibrary={getLibrary}>
+            <div className="flex flex-col min-h-full">
+                <Navbar />
+                <div className="main-wrapper">
+                    <Head>
+                        <title>
+                            Yellow DeFi - Discover WEB 3.0 Internet of Finance
+                        </title>
+                        <meta
+                            name="description"
+                            content="Yellow DeFi is a new generation hybrid technology cryptocurrency exchange combining the best of decentralized and centralized performance."
+                        />
+                    </Head>
+                    <main className="duckies container">
+                        <DuckiesHero />
+                        <DuckiesAffiliates />
+                        <DuckiesEarnMore />
+                        <DuckiesRedeem />
+                    </main>
+                </div>
+                <Footer />
             </div>
-            <Footer />
-        </div>
+        </Web3ReactProvider>
     );
 };
 
