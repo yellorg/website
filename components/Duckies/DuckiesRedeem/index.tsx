87d777bb9766657a434069e24b2a9d596e7ef02b
Oleksandr Khlopiachyi <akhlopiachyi@openware.com>
2022-06-01 01:27:55 +0700

    feat: add section to redeem section

--- components/Duckies/DuckiesRedeem/index.tsx
+++ components/Duckies/DuckiesRedeem/index.tsx
@@ -71,15 +71,53 @@ const NFT_LIST = [
 export const DuckiesRedeem = () => {
     return (
         <div className="duckies-redeem">
-            <div className="container">
-                <div className="duckies-redeem__header">
-                    <div className="duckies-redeem__header-title">
-                        Redeem
+            <div className="container duckies-redeem__block">
+                <div className="duckies-redeem__block-info">
+                    <div className="duckies-redeem__block-info-header">
+                        <div className="duckies-redeem__block-info-header-name">
+                            <div className="duckies-redeem__block-info-header-name-content">
+                                DUCKIES
+                            </div>
+                        </div>
+                        <div className="duckies-redeem__block-info-header-title">
+                            Redeem
+                        </div>
+                        <div className="duckies-redeem__block-info-header-subtitle">
+                            Exchange your duckies for valuable gifts and rare NFTs
+                        </div>
                     </div>
-                    <div className="duckies-redeem__header-subtitle">
-                        Exchange your duckies for valuable gifts and rare NFTs
+                    <div className="duckies-redeem__block-info-content">
+                        <div className="duckies-redeem__block-info-content-row">
+                            <div className="duckies-redeem__block-info-content-row-item">
+                            <div className="duckies-redeem__block-info-content-row-item-line" />
+                                Exclusive NFT Duckies collection
+                            </div>
+                            <div className="duckies-redeem__block-info-content-row-item">
+                                <div className="duckies-redeem__block-info-content-row-item-line" />
+                                Swap for YELLOW tokens
+                            </div>
+                        </div>
+                        <div className="duckies-redeem__block-info-content-row">
+                            <div className="duckies-redeem__block-info-content-row-item">
+                            <div className="duckies-redeem__block-info-content-row-item-line" />
+                                YELLOW NETWORK & DUCKIES merch
+                            </div>
+                            <div className="duckies-redeem__block-info-content-row-item">
+                            <div className="duckies-redeem__block-info-content-row-item-line" />
+                                And more coming soon
+                            </div>
+                        </div>
                     </div>
                 </div>
+                <div className="duckies-redeem__block-duckies">
+                    <LazyLoadImage className="section__main-img"
+                        srcSet={`${'/images/components/duckies/redeem.svg'}`}
+                        effect="blur"
+                        threshold={200}
+                    />
+                </div>
+            </div>
+            <div className="container">
                 <div className="duckies-redeem__slider">
                     <Slider {...SLIDER_SETTINGS}>
                         {NFT_LIST.map((imgSrc, i) => (
