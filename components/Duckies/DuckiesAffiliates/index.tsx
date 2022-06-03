f109f28db285d900b37207335e1f76a392df0f3a
Alex Khlopiachyi <akhlopiachyi@openware.com>
2022-06-03 14:51:56 +0700

    feat: add claiming of bounty (#36)

--- components/Duckies/DuckiesAffiliates/index.tsx
+++ components/Duckies/DuckiesAffiliates/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import useDuckiesContract from '../../../hooks/useDuckiesContract';
 import useWallet from '../../../hooks/useWallet';
-import PrismicSliceZone from '../../PrismicSliceZone';
+import { BountyItem, BountyRow } from '../BountyRow';
 
 interface DuckiesAffiliatesProps {
     bounties: any;
@@ -10,6 +10,8 @@ interface DuckiesAffiliatesProps {
 export const DuckiesAffiliates: React.FC<DuckiesAffiliatesProps> = ({ bounties }: DuckiesAffiliatesProps) => {
     const [affiliates, setAffiliates] = React.useState<number[]>([0, 0, 0, 0, 0]);
     const [payouts, setPayouts] = React.useState<number[]>([]);
+    const { items } = bounties.data.slices[0];
+    const [bountyItems, setBountyItems] = React.useState<any[]>([]);
 
     const duckiesContract = useDuckiesContract();
     const { active, account, signer } = useWallet();
@@ -50,6 +52,127 @@ export const DuckiesAffiliates: React.FC<DuckiesAffiliatesProps> = ({ bounties }
         });
     }, [affiliates, payouts]);
 
+    const handleClaimReward = React.useCallback(async (id: string) => {
+        const bountyToClaim = bountyItems.find((item: BountyItem) => item.fid === id);
+
+        if (bountyToClaim && signer) {
+            const { transaction } = await (await fetch(
+                `/api/bountyTx?bountyID=${bountyToClaim.fid}&&account=${account}`
+            )).json();
+
+            try {
+                const tx = await signer.sendTransaction(transaction);
+                await tx.wait();
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }, [signer, bountyItems, account]);
+
+    const getAffiliatesCountOnLevel = React.useCallback((level: string, value: string): boolean => {
+        return affiliates[+level - 1] < +value ? false : true;
+    }, [affiliates]);
+
+    const getAffiliatesRuleCompleted = React.useCallback((level: string, key: string, value: string): boolean => {
+        switch (key) {
+            case 'count':
+                const [_, levelNumber] = level.split('-');
+
+                return getAffiliatesCountOnLevel(levelNumber, value);
+            default:
+                return false;
+        }
+    }, [getAffiliatesCountOnLevel]);
+
+    const getClaimedBountyInfo = React.useCallback(async (bounty: any) => {
+        if (signer) {
+            const bountyId = bounty.fid.split('-')[0];
+            const claimedTimes = await duckiesContract?.connect(signer).getAccountBountyLimit(bounty.fid);
+            let status = '';
+
+            switch (bountyId) {
+                case 'affiliates':
+                    const [level, key, value] = bounty.triggerPhrase.split(' ');
+                    const result = getAffiliatesRuleCompleted(level, key, value);
+
+                    if (claimedTimes === bounty.limit) {
+                        status = 'claimed';
+                    } else {
+                        if (result) {
+                            status = 'claim';
+                        }
+                    }
+                    break;
+                default:
+            }
+
+            const bountyIndex = bountyItems.findIndex((item => item.fid === bounty.fid));
+
+            if (bountyIndex !== -1 && bountyItems[bountyIndex].status !== status) {
+                const newBountyItems = [...bountyItems];
+
+                newBountyItems[bountyIndex] = {
+                    ...bounty,
+                    status,
+                }
+                setBountyItems(newBountyItems);
+            }
+        }
+        return 0;
+    }, [duckiesContract, signer, bountyItems, getAffiliatesRuleCompleted]);
+
+    React.useEffect(() => {
+        if (items) {
+            const newItems = items.map((item: any) => {
+                return {
+                    ...item,
+                    status: '',
+                }
+            });
+
+            setBountyItems(newItems);
+        }
+    }, [items]);
+
+    React.useEffect(() => {
+        if (bountyItems.length) {
+            bountyItems.forEach((bounty: any) => {
+                getClaimedBountyInfo(bounty);
+            });
+        }
+    }, [bountyItems, getClaimedBountyInfo]);
+
+    const renderBountySlices = React.useMemo(() => {
+        return bountyItems.map((bounty: BountyItem, index: number) => {
+            return (
+                <React.Fragment key={`bounty-${bounty.fid}`}>
+                    <BountyRow bounty={bounty} handleClaim={handleClaimReward} index={index + 1} />
+                </React.Fragment>
+            );
+        });
+    }, [bountyItems, handleClaimReward]);
+
+    const bountiesToClaim = React.useMemo(() => {
+        return bountyItems
+            .filter(bounty => bounty.status === 'claim')
+            .map(item => item.fid);
+    }, [bountyItems]);
+
+    const handleClaimAllBounties = React.useCallback(async () => {
+        if (bountiesToClaim.length && signer) {
+            const { transaction } = await (await fetch(
+                `/api/allBountiesTx?bountyIDs=${bountiesToClaim}&account=${account}`
+            )).json();
+
+            try {
+                const tx = await signer.sendTransaction(transaction);
+                await tx.wait();
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }, [signer, account, bountiesToClaim]);
+
     return (
         <div className="duckies-affiliates">
             <div className="container">
@@ -66,11 +189,13 @@ export const DuckiesAffiliates: React.FC<DuckiesAffiliatesProps> = ({ bounties }
                         <div className="table">
                             <div className="table-title">
                                 {bounties.data.title}
-                                <div onClick={() => window.console.log('claim all')} className="button button--outline button--secondary button--shadow-secondary">
-                                    <span className="button__inner">Claim All</span>
-                                </div>
+                                {bountiesToClaim.length > 1 && (
+                                    <div onClick={handleClaimAllBounties} className="button button--outline button--secondary button--shadow-secondary">
+                                        <span className="button__inner">Claim All</span>
+                                    </div>)
+                                }
                             </div>
-                            <PrismicSliceZone slices={bounties.data.slices} />
+                            {renderBountySlices}
                         </div>
                     </div>
                 </div>
---
+++ pages/api/bountyTx.ts
@@ -0,0 +1,73 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import jwt from 'jsonwebtoken';
+import { ethers } from 'ethers';
+import DuckiesContractBuild from '../../contracts/artifacts/contracts/Duckies.sol/Duckies.json';
+import Web3 from 'web3';
+import { createClient } from '../../prismicio';
+
+const getBountyTransactionObject = async (bountyID: string, account: string) => {
+    const client = createClient();
+    const bounties = await client.getSingle('bounties');
+    const bountyToClaim = bounties.data.slices[0].items.find((item: any) => item.fid === bountyID);
+
+    if (!bountyToClaim) {
+      throw { code: 404, message: `bounty with id ${bountyID} not found` };
+    }
+
+    const privateKey = process.env.NEXT_PUBLIC_METAMASK_PRIVATE_KEY || '';
+    const contractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || '';
+
+    const provider = new ethers.providers.JsonRpcProvider(process.env.NEXT_PUBLIC_INFURA_URL);
+    const web3 = new Web3(new Web3.providers.HttpProvider(process.env.NEXT_PUBLIC_INFURA_URL || ''));
+
+    const contract = new ethers.Contract(
+        contractAddress,
+        DuckiesContractBuild.abi,
+        provider,
+    );
+
+    const decimals = await contract.decimals();
+    const currentBlock = await (provider as any).getBlock();
+    const rewardMessage = {
+        id: bountyID,
+        amt: +bountyToClaim.value * 10 ** decimals,
+        limit: bountyToClaim.limit,
+        blockExpiration: currentBlock.number + 12,
+        ref: '0x0000000000000000000000000000000000000000',
+    };
+
+    const nonce = await web3.eth.getTransactionCount(account);
+    const rewardMessageHash = await contract.getMessageHash(rewardMessage);
+    const signature = await web3.eth.accounts.sign(rewardMessageHash, privateKey);
+
+    const iface = new ethers.utils.Interface(DuckiesContractBuild.abi);
+    const data = iface.encodeFunctionData('reward', [[rewardMessage.id, rewardMessage.ref, rewardMessage.amt, rewardMessage.blockExpiration, rewardMessage.limit], signature.signature]);
+
+    const initialTransaction = {
+        from: account,
+        nonce,
+        to: contractAddress,
+        data,
+    };
+
+    const estimatedGas = await web3.eth.estimateGas(initialTransaction);
+
+    const transaction = {
+        ...initialTransaction,
+        gasLimit: estimatedGas,
+    };
+
+    return {
+        transaction,
+    };
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    try {
+        const tx = await getBountyTransactionObject(req.query.bountyID as string, req.query.account as string);
+
+        res.status(200).json(tx);
+    } catch (error: any) {
+        res.status(500).json({ error: error.message });
+    }
+}
