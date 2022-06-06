47390b58f2a227530ea37659be569a6715c8493a
Oleksandr Khlopiachyi <akhlopiachyi@openware.com>
2022-06-06 23:36:03 +0700

    feat: update gtm id

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,12 +10,12 @@ export default function Document() {
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-KMLXRGV');`}}></script>
+        })(window,document,'script','dataLayer','GTM-MKQHGKN');`}}></script>
         {/* <!-- End Google Tag Manager --> */}
    </Head>
         <body>
             {/* <!-- Google Tag Manager (noscript) --> */}
-            <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KMLXRGV"
+            <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MKQHGKN"
               height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
             {/* // <!-- End Google Tag Manager (noscript) --> */}
           <Main />
