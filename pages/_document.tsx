bbdd2913859ba2bc625b5d262473902957c6331c
Oleksandr Khlopiachyi <akhlopiachyi@openware.com>
2022-06-06 22:41:20 +0700

    fix: google tag manager

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,24 +1,26 @@
-import { GTM_ID } from '../lib/gtm'
-import Document, { Head, Html, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
-export default class MyDocument extends Document {
-    render() {
-        return (
-            <Html>
-                <Head />
-                <body>
-                    <noscript>
-                        <iframe
-                            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-                            height="0"
-                            width="0"
-                            style={{ display: 'none', visibility: 'hidden' }}
-                        />
-                    </noscript>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-    }
+export default function Document() {
+   return (
+     <Html>
+        <Head>
+      <link rel="shortcut icon" href="/favicon.ico" />
+        {/* <!-- Google Tag Manager --> */}
+        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KMLXRGV');`}}></script>
+        {/* <!-- End Google Tag Manager --> */}
+   </Head>
+        <body>
+            {/* <!-- Google Tag Manager (noscript) --> */}
+            <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KMLXRGV"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
+            {/* // <!-- End Google Tag Manager (noscript) --> */}
+          <Main />
+          <NextScript />
+        </body>
+     </Html>
+  );
 }
