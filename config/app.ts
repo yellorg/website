faeed2b665e4fac68542c23969c3ce89bda471e2
Oleksandr Khlopiachyi <akhlopiachyi@openware.com>
2022-06-07 19:49:40 +0700

    fix: uncomment mainnet polygon chain

--- config/app.ts
+++ config/app.ts
@@ -2,9 +2,7 @@ export const appConfig = {
   appName: 'Duckies',
   blockchain: {
     supportedChainIds: [
-      // 1, // Ethereum Mainnet
-      4, // Ethereum Testnet Rinkeby
-      // 137, // Polygon Mainnet
+      137, // Polygon Mainnet
       80001, // Polygon Testnet Mumbai
     ],
   },
