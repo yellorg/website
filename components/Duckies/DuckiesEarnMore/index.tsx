fe83f17af3f774a6245c96824fce2d651288b92a
MaxMoskalenko <mx.msklnk@gmail.com>
2022-06-28 09:37:00 +0300

    fix: identation

--- components/Duckies/DuckiesEarnMore/index.tsx
+++ components/Duckies/DuckiesEarnMore/index.tsx
@@ -348,16 +348,20 @@ export const DuckiesEarnMore = () => {
                                 </div>
                                 <div className={inputLink} />
                             </div>
-                            {isReady ?
-                            <div onClick={() => handleCopy(`${shareableLinkPrefix}${shareableLink}`)} className="button button--outline button--secondary button--shadow-secondary">
-                                <span className="button__inner">
-                                    <svg width="20" height="24" viewBox="0 0 20 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M8.60019 0.800049C7.0538 0.800049 5.8002 2.05365 5.8002 3.60005V14.8C5.8002 16.3464 7.0538 17.6 8.60019 17.6H17.0002C18.5466 17.6 19.8002 16.3464 19.8002 14.8V6.97995C19.8002 6.23734 19.5052 5.52515 18.9801 5.00005L15.6002 1.62015C15.0751 1.09505 14.3629 0.800049 13.6203 0.800049H8.60019Z" fill="black"/>
-                                        <path d="M0.200195 9.20005C0.200195 7.65365 1.4538 6.40005 3.0002 6.40005V20.4001H14.2002C14.2002 21.9464 12.9466 23.2001 11.4002 23.2001H3.0002C1.4538 23.2001 0.200195 21.9464 0.200195 20.4001V9.20005Z" fill="black"/>
-                                    </svg>
-                                </span>
-                            </div>
-                            : <div className="duckies-earn-more__body-link-input-image"><img src="/images/components/duckies/login_eyes.png" alt="login" /></div>}
+                            {isReady ? (
+                                <div onClick={() => handleCopy(`${shareableLinkPrefix}${shareableLink}`)} className="button button--outline button--secondary button--shadow-secondary">
+                                    <span className="button__inner">
+                                        <svg width="20" height="24" viewBox="0 0 20 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M8.60019 0.800049C7.0538 0.800049 5.8002 2.05365 5.8002 3.60005V14.8C5.8002 16.3464 7.0538 17.6 8.60019 17.6H17.0002C18.5466 17.6 19.8002 16.3464 19.8002 14.8V6.97995C19.8002 6.23734 19.5052 5.52515 18.9801 5.00005L15.6002 1.62015C15.0751 1.09505 14.3629 0.800049 13.6203 0.800049H8.60019Z" fill="black"/>
+                                            <path d="M0.200195 9.20005C0.200195 7.65365 1.4538 6.40005 3.0002 6.40005V20.4001H14.2002C14.2002 21.9464 12.9466 23.2001 11.4002 23.2001H3.0002C1.4538 23.2001 0.200195 21.9464 0.200195 20.4001V9.20005Z" fill="black"/>
+                                        </svg>
+                                    </span>
+                                </div>
+                            ) : (
+                                <div className="duckies-earn-more__body-link-input-image">
+                                    <img src="/images/components/duckies/login_eyes.png" alt="login" />
+                                </div>
+                            )}
                         </div>
                         <div className={classnames('duckies-earn-more__body-social', { 'center-align': !isReady, 'left-align': isReady })}>
                             {isReady ? renderSocials : renderMetamaskButton()}
