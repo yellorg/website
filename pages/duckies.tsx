d8e17d7c21d37c4396989128120904e6e56ba741
MaxMoskalenko <mx.msklnk@gmail.com>
2022-06-30 13:10:36 +0300

    fix: add metatags for og and twitter preview (#52)

--- pages/duckies.tsx
+++ pages/duckies.tsx
@@ -44,6 +44,38 @@ const Duckies: FC = ({ bounties }: any): JSX.Element => {
                                 name="description"
                                 content="Yellow DeFi is a new generation hybrid technology cryptocurrency exchange combining the best of decentralized and centralized performance."
                             />
+                            <meta 
+                                property="og:type"
+                                content="website"
+                            />
+                            <meta
+                                property="og:title"
+                                content="DUCKIES—The Fun and Friendly Web3 Currency"
+                            />
+                            <meta
+                                property="og:description"
+                                content="The DUCKIES token is a decentralized meme coin and the Yellow community currency for true growth hackers. Join the duckies squad! Quack-quack!"
+                            />
+                            <meta
+                                property="og:image"
+                                content="/images/og-image.png"    
+                            />
+                            <meta
+                                name="twitter:card"
+                                content="summary_large_image"
+                            />
+                            <meta
+                                name="twitter:title"
+                                content="DUCKIES—The Fun and Friendly Web3 Currency"
+                            />
+                            <meta
+                                name="twitter:description"
+                                content="The DUCKIES token is a decentralized meme coin and the Yellow community currency for true growth hackers. Join the duckies squad! Quack-quack!"
+                            />
+                            <meta
+                                name="twitter:image"
+                                content="https://www.yellow.org/images/og-image.png"    
+                            />
                         </Head>
                         <DuckiesLayout bounties={bounties} />
                     </div>
