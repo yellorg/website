cc6d402ac36cd84b782e4b48e72d2d459304fced
Nikita Zaikin <89794266+nzaikin@users.noreply.github.com>
2022-07-05 14:50:31 +0300

    fix(duckies): update footer + delete unnecessary comments (#75)

--- components/IconExternalLink/index.tsx
+++ components/IconExternalLink/index.tsx
@@ -11,12 +11,13 @@ function IconExternalLink({width = 13.5, height = 13.5, className = 'iconExterna
     <svg
       width={width}
       height={height}
-      aria-hidden="true"
-      viewBox="0 0 24 24"
-      className={className}>
+      viewBox="0 0 14 14"
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+    >
       <path
-        fill="currentColor"
-        d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"
+        d="M12.25 12.25H1.75V1.75H7V0.25H1.75C0.9175 0.25 0.25 0.925 0.25 1.75V12.25C0.25 13.075 0.9175 13.75 1.75 13.75H12.25C13.075 13.75 13.75 13.075 13.75 12.25V7H12.25V12.25ZM8.5 0.25V1.75H11.1925L3.82 9.1225L4.8775 10.18L12.25 2.8075V5.5H13.75V0.25H8.5Z"
+        fill="white"
       />
     </svg>
   );
