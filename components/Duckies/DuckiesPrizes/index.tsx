b79f3e876cbaf87cbd3a11b876d673317699ae2a
Nikita Zaikin <nzaikin@openware.center>
2022-07-12 00:04:43 +0300

    fix(duckies): update styles for prizes cards

--- components/Duckies/DuckiesPrizes/index.tsx
+++ components/Duckies/DuckiesPrizes/index.tsx
@@ -21,85 +21,108 @@ export const DuckiesPrizes = () => {
                         </div>
                     </div>
                     <div className="flex overflow-visible block">
-                        <Tilt
-                            perspective={500}
-                            scale={0.9}
-                            tiltMaxAngleX={10}
-                            tiltMaxAngleY={10}
-                        >
-                            <div className="flex mr-5 w-[25.5rem] h-[35.813rem] border-2 border-text-color-100 items-center rounded-lg bg-primary-cta-color-80 drop-shadow-[0_4.625rem_2.125rem_rgba(166,87,14,0.24)]">
-                                <LazyLoadImage
-                                    srcSet={`${'/images/components/duckies/duck.png'}`}
-                                    effect="blur"
-                                    threshold={200}
-                                />                  
-                                <span className="absolute bottom-0 right-0 m-4 font-metro-semibold">NFT COLLECTION #1</span>
-                                <LazyLoadImage
-                                    srcSet={`${'/images/components/duckies/sparkles.gif'}`}
-                                    threshold={200}
-                                    className="absolute mix-blend-color-dodge opacity-50 w-full h-full"
-                                />
-                                <LazyLoadImage
-                                    srcSet={`${'/images/components/duckies/holo.webp'}`}
-                                    threshold={200}
-                                    className="absolute mix-blend-color-dodge opacity-20 w-full h-full"
-                                />
-                                <LazyLoadImage
-                                    srcSet={`${'/images/components/duckies/coming-soon.gif'}`}
-                                    threshold={200}
-                                    className="absolute bottom-16"
-                                />
-                                <div className="hover:hidden w-full h-full absolute">
+                        <div className="mr-5">
+                            <Tilt
+                                perspective={500}
+                                scale={0.9}
+                                tiltMaxAngleX={10}
+                                tiltMaxAngleY={10}
+                                glareEnable={true} 
+                                glareMaxOpacity={0.3}
+                                className="group"
+                            >
+                                <div className="flex w-[25.5rem] h-[35.813rem] border-2 border-text-color-100 items-center rounded-lg bg-primary-cta-color-80 drop-shadow-[0_4.625rem_2.125rem_rgba(166,87,14,0.24)]">
                                     <LazyLoadImage
-                                        srcSet={`${'/images/components/duckies/coming-soon-static.png'}`}
+                                        srcSet={`${'/images/components/duckies/duck.png'}`}
+                                        effect="blur"
                                         threshold={200}
-                                        className="absolute bottom-12"
-                                    />
-                                </div>
-                            </div>
-                        </Tilt>
-                        <Tilt
-                            perspective={500}
-                            scale={0.9}
-                            tiltMaxAngleX={10}
-                            tiltMaxAngleY={10}
-                        >
-                            <div className="flex mr-5 w-[25.5rem] h-[35.813rem] border-2 border-text-color-100 items-center rounded-lg bg-primary-cta-color-80 drop-shadow-[0_4.625rem_2.125rem_rgba(166,87,14,0.24)]">
-                                <LazyLoadImage
-                                    srcSet={`${'/images/components/duckies/duckBusiness.png'}`}
-                                    effect="blur"
-                                    threshold={200}
-                                />
-                                <LazyLoadImage
-                                    srcSet={`${'/images/components/duckies/coming-soon.gif'}`}
-                                    threshold={200}
-                                    className="absolute bottom-16"
-                                />
-                                <span className="absolute bottom-0 right-0 m-4 font-metro-semibold">SWAPPING FOR YELLOW TOKENS</span>
-                                <LazyLoadImage
-                                    srcSet={`${'/images/components/duckies/sparkles.gif'}`}
-                                    threshold={200}
-                                    className="absolute mix-blend-color-dodge opacity-50 w-full h-full"
-                                />
-                                <LazyLoadImage
-                                    srcSet={`${'/images/components/duckies/holo.webp'}`}
-                                    threshold={200}
-                                    className="absolute mix-blend-color-dodge opacity-20 w-full h-full"
-                                />
-                                <div className="hover:hidden w-full h-full absolute">
+                                    />                  
+                                    <span className="absolute bottom-0 right-0 m-4 font-metro-semibold">NFT COLLECTION #1</span>
                                     <LazyLoadImage
-                                        srcSet={`${'/images/components/duckies/coming-soon-static.png'}`}
+                                        srcSet={`${'/images/components/duckies/sparkles.gif'}`}
                                         threshold={200}
-                                        className="absolute bottom-12"
+                                        className="absolute mix-blend-color-dodge opacity-50 w-full h-full"
                                     />
+                                    <LazyLoadImage
+                                        srcSet={`${'/images/components/duckies/holo.webp'}`}
+                                        threshold={200}
+                                        className="absolute mix-blend-color-dodge opacity-20 w-full h-full"
+                                    />
+                                    <LazyLoadImage
+                                        srcSet={`${'/images/components/duckies/coming-soon.gif'}`}
+                                        threshold={200}
+                                        className="absolute bottom-16"
+                                    />
+                                    <div className="group-hover:hidden w-full h-full absolute">
+                                        <LazyLoadImage
+                                            srcSet={`${'/images/components/duckies/coming-soon-static.png'}`}
+                                            threshold={200}
+                                            className="absolute bottom-12"
+                                        />
+                                    </div>
                                 </div>
-                            </div>
-                        </Tilt>
+                            </Tilt>
+                        </div>
+                        <div className="mr-5">
+                            <Tilt
+                                perspective={500}
+                                scale={0.9}
+                                tiltMaxAngleX={10}
+                                tiltMaxAngleY={10}
+                                glareEnable={true} 
+                                glareMaxOpacity={0.3}
+                                className="group"
+                            >
+                                <div className="flex w-[25.5rem] h-[35.813rem] border-2 border-text-color-100 items-center rounded-lg bg-primary-cta-color-80 drop-shadow-[0_4.625rem_2.125rem_rgba(166,87,14,0.24)]">
+                                    <LazyLoadImage
+                                        srcSet={`${'/images/components/duckies/duckBusiness.png'}`}
+                                        effect="blur"
+                                        threshold={200}
+                                    />
+                                    <LazyLoadImage
+                                        srcSet={`${'/images/components/duckies/coming-soon.gif'}`}
+                                        threshold={200}
+                                        className="absolute bottom-16"
+                                    />
+                                    <div className="flex bottom-0 right-0 m-4 absolute inline-block">
+                                        <span className="font-metro-semibold">SWAPPING FOR YELLOW </span>
+                                        <LazyLoadImage
+                                        srcSet={`${'/images/components/duckies/yellow-token.png'}`}
+                                        threshold={30}
+                                        width={25}
+                                        height={25}
+                                        className="mx-1"
+                                        />
+                                        <span className="font-metro-semibold">TOKENS</span>
+                                    </div>
+                                    <LazyLoadImage
+                                        srcSet={`${'/images/components/duckies/sparkles.gif'}`}
+                                        threshold={200}
+                                        className="absolute mix-blend-color-dodge opacity-50 w-full h-full"
+                                    />
+                                    <LazyLoadImage
+                                        srcSet={`${'/images/components/duckies/holo.webp'}`}
+                                        threshold={200}
+                                        className="absolute mix-blend-color-dodge opacity-20 w-full h-full"
+                                    />
+                                    <div className="group-hover:hidden w-full h-full absolute">
+                                        <LazyLoadImage
+                                            srcSet={`${'/images/components/duckies/coming-soon-static.png'}`}
+                                            threshold={200}
+                                            className="absolute bottom-12"
+                                        />
+                                    </div>
+                                </div>
+                            </Tilt>
+                        </div>
                         <Tilt
                             perspective={500}
                             scale={0.9}
                             tiltMaxAngleX={10}
                             tiltMaxAngleY={10}
+                            glareEnable={true} 
+                            glareMaxOpacity={0.3}
+                            className="group"
                         >
                             <div className="flex w-[25.5rem] h-[35.813rem] border-2 border-text-color-100 items-center rounded-lg bg-primary-cta-color-80 drop-shadow-[0_4.625rem_2.125rem_rgba(166,87,14,0.24)]">
                                 <LazyLoadImage
@@ -123,7 +146,7 @@ export const DuckiesPrizes = () => {
                                     threshold={200}
                                     className="absolute mix-blend-color-dodge opacity-20 w-full h-full"
                                 />
-                                <div className="hover:hidden w-full h-full absolute">
+                                <div className="group-hover:hidden w-full h-full absolute">
                                     <LazyLoadImage
                                         srcSet={`${'/images/components/duckies/coming-soon-static.png'}`}
                                         threshold={200}
