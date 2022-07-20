16848190a9ee0a98d2209bde457c5a3658335730
Nikita Zaikin <nzaikin@openware.center>
2022-07-20 20:51:51 +0300

    fix(yellow-org): add socials to navbar

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,16 +1,54 @@
 import classnames from 'classnames';
 import Link from 'next/link';
 import Image from 'next/image';
-import React from 'react';
+import React, { useEffect } from 'react';
 import IconClose from '../IconClose';
 import IconMenu from '../IconMenu';
 import { NAV_ITEMS } from './navItems';
 
 export const Navbar = () => {
     const [showSidebar, setShowSidebar] = React.useState<boolean>(false);
+    const [isSocialsOpen, setIsSocialsOpen] = React.useState<boolean>(false);
+
+    const socialsRef = React.useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        document.addEventListener('mousedown', (event) => {
+            if (socialsRef.current !== null && !socialsRef.current.contains(event.target as Node)) {
+                setIsSocialsOpen(false);
+            }
+        });
+    }),[]
+
+    const renderSocials = React.useMemo(() => {
+        const { socials } = NAV_ITEMS;
+        return (
+            <div className="flex justify-center">
+                {socials.map( (social, i) => {
+                    return (
+                        <Link key={i} href={social.to}>
+                            <a
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="mx-1"
+                            >
+                                <Image
+                                    src={social.src}
+                                    alt="social"
+                                    loading="lazy"
+                                    width={24}
+                                    height={24}
+                                />
+                            </a>
+                        </Link>
+                    )
+                })}
+            </div>
+        );
+    }, []);
 
     const renderNavItems = React.useMemo(() => {
-        return NAV_ITEMS.map((n, i) => {
+        return NAV_ITEMS.navTabs.map((n, i) => {
             return (
                 <Link key={i} href={n.to}>
                     <a
@@ -25,13 +63,12 @@ export const Navbar = () => {
     }, []);
 
     const renderMobileNavItems = React.useMemo(() => {
-        return NAV_ITEMS.map((n, i) => {
+        return NAV_ITEMS.navTabs.map((n, i) => {
             return (
-                <li key={i} className="menu__list-item">
+                <li key={i} className="pr-2">
                     <Link href={n.to}>
                         <a
                             aria-current="page"
-                            className="menu__link menu__link--active"
                         >
                             {n.label}
                         </a>
@@ -43,6 +80,10 @@ export const Navbar = () => {
 
     const cnNav = classnames('flex shadow-sm justify-center items-center h-[62px] sticky top-0 z-[22] bg-primary-cta-color-60', {
         'navbar-sidebar--show': showSidebar,
+    });
+
+    const cnSocialsArrow = classnames('h-4 w-4 mt-1', {
+        'rotate-180': isSocialsOpen,
     });
 
     return (
@@ -75,6 +116,28 @@ export const Navbar = () => {
                     {renderNavItems}
                 </div>
                 <div className="flex items-center min-w-0 justify-end">
+                    <div ref={socialsRef} className="relative invisible md:visible inline-block text-center mr-3">
+                        <div>
+                            <button 
+                                type="button" 
+                                className="inline-flex w-full items-center mr-5 justify-center w-full bg-primary-cta-color-60 text-md font-metro-semibold focus:outline-none" 
+                                id="menu-button"
+                                onClick={() => setIsSocialsOpen((isSocialsOpen) => !isSocialsOpen)}
+                            >
+                                Socials
+                                <svg className={cnSocialsArrow} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                </svg>
+                            </button>
+                        </div>
+                        {isSocialsOpen &&
+                            <div className="origin-top-right absolute right-0 mt-4 shadow-lg bg-white focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button">
+                                <div className="pt-3 pb-2 px-3" role="none">
+                                    {renderSocials}
+                                </div>
+                            </div>
+                        }
+                    </div>
                     <a
                         className="button button--secondary"
                         href="https://docs.yellow.org/whitepaper"
--- components/Navbar/navItems.js
+++ components/Navbar/navItems.js
@@ -1,7 +1,16 @@
-export const NAV_ITEMS = [
-    {to: '/#about', label: 'About'},
-    {to: '/#tokenomics', label: 'Token'},
-    {to: '/#roadmap', label: 'Roadmap'},
-    {to: '/#team', label: 'Team'},
-    {to: '/#partners', label: 'Partners'},
-]
+export const NAV_ITEMS = {
+    socials: [
+        { src: '/images/icons/reddit-nav-black.svg', to: 'https://www.reddit.com/r/YellowDeFi/' },
+        { src: '/images/icons/twitter-nav-black.svg', to: 'https://twitter.com/Yellow' },
+        { src: '/images/icons/medium-nav-black.svg', to: 'https://medium.com/yellow-blog' },
+        { src: '/images/icons/telegram-nav-black.svg', to: 'https://t.me/yellow_org' },
+        { src: '/images/icons/discord-nav-black.svg', to: 'https://discord.com/invite/DKBjCg6rmT' },
+    ],
+    navTabs: [
+        {to: '/#about', label: 'About'},
+        {to: '/#tokenomics', label: 'Token'},
+        {to: '/#roadmap', label: 'Roadmap'},
+        {to: '/#team', label: 'Team'},
+        {to: '/#partners', label: 'Partners'},
+    ],
+}
